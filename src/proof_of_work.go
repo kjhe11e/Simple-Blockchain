d873bb8aa1ee006b5e68cb7f3f60583d2cca0697
Kyle Helle <kyle@coinme.com>
2018-03-11 19:36:44 -0700

    add basic proof of work

---
+++ src/proof_of_work.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"math/big"
+	"bytes"
+	"fmt"
+	"crypto/sha256"
+	"math"
+)
+
+var (
+	maxNonce = math.MaxInt64
+)
+
+const MAX_TARGET_SIZE = 256
+const TARGET_BITS = 24
+
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(MAX_TARGET_SIZE-TARGET_BITS))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(TARGET_BITS)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
